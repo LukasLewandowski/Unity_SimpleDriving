d7475afe75b8afbb48dabfb155ef982119aad665
Lukas Lewandowski <llewandowski1990@gmail.com>
2021-10-17 22:16:59 +0200

    add steering

--- Assets/Sripts/Car.cs
+++ Assets/Sripts/Car.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speed = 2f;
     [SerializeField] private float speedGainedPerSecond = 0.2f;
+    [SerializeField] private float turnSpeed = 50f;
+
+    private int steerValue;
 
     // Update is called once per frame
     void Update()
     {
-        /** * Time.deltaTime it's how long since the last frame */
+        /** moving forward: * Time.deltaTime it's how long since the last frame */
         speed = speed + speedGainedPerSecond * Time.deltaTime;
         transform.Translate(Vector3.forward * speed * Time.deltaTime);   
+
+        /** steering */
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+    }
+        public void Steer(int value) {
+        steerValue = value;
     }
 }
