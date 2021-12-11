b62e18682a418dbf9321b2042f0e50597d11e0bd
Lukas Lewandowski <llewandowski1990@gmail.com>
2021-12-12 00:27:10 +0100

    working on race track

--- Assets/Sripts/Car.cs
+++ Assets/Sripts/Car.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Car : MonoBehaviour
 {
@@ -11,14 +12,20 @@ public class Car : MonoBehaviour
     private int steerValue;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         /** moving forward: * Time.deltaTime it's how long since the last frame */
         speed = speed + speedGainedPerSecond * Time.deltaTime;
         transform.Translate(Vector3.forward * speed * Time.deltaTime);   
 
         /** steering */
         transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.CompareTag("Obstacle") == true) {
+            /** load first scene which is Main_Menu */
+            SceneManager.LoadScene(0);
+        }
     }
         public void Steer(int value) {
         steerValue = value;
